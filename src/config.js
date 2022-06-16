c9aec551554556ee92694eaa6bb956a06279c3e6
MaxAirFrance <107645084+MaxAirFrance@users.noreply.github.com>
2022-06-16 20:35:39 +0200

    Initial commit

---
+++ src/config.js
@@ -0,0 +1,17 @@
+//////////////////////////////////////
+//  CONFIG
+//////////////////////////////////////
+
+export default {
+    id       : "Emauzium", // Name of your server (will be the DB name)
+    token    : "a51483a4ddcc32368b38dc38744d0ad0d93c8f284f5bfe0ddbc3e1833e6c9fb9", // Your bot token
+    websea   : "", // Link where to send the edit webhook embed
+    webcre   : "", // Link where to send the create webhook embed
+    webedi   : "", // Link where to send the edit webhook embed
+    webdel   : "", // Link where to send the delete webhook embed
+    permsup  : "964535752388120586", // ID of the role who can delete movies
+    rolehost : "964535752388120586", // ID of the role who can add movies
+    chlist   : "", // Link to the server channel where the list is diplayed
+    chsugg   : "", // Link to the server channel where user can ask for a movie
+    icon     : "" // Server icon (PNG/JPEG only)
+}
